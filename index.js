8f244fdc8ce3ed3f0a5708cc7f55c871ff3320b2
jeongmi-min <alswjdalwjal@naver.com>
2025-05-12 11:41:57 +0900

    main

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 const modal = document.getElementById("modal");
 const modalCloseButton = document.getElementById("modal-close-btn");
-const contentForm = document.getElementById("contnent-form");
+const contentForm = document.getElementById("content-form");
 const modalText = document.getElementById("modal-text");
 const modalSubmitBtn = document.getElementById("modal-submit-btn");
 const modalResetBtn = document.getElementById("modal-reset-btn");
 
 setTimeout(function () {
-  (modal.style.display = "inline"), 1500;
-});
+  modal.style.display = "inline";
+}, 1500);
 
 modalCloseButton.addEventListener("click", function () {
   modal.style.display = "none";
 });
 
-contentForm.addEventListener("submit", function (event) {
-  event.preventDefault;
+modalSubmitBtn.addEventListener("click", function (event) {
+  event.preventDefault();
 
   const contentFormData = new FormData(contentForm);
+  const name = contentFormData.get("fullName");
+  const email = contentFormData.get("email");
+  const number = contentFormData.get("phoneNumber");
+
+  modalText.innerHTML = `<div class="modal-inner-loading">
+   <img src="images/loading.svg" class="loading">
+   <p id="upload-text">Uploading your data to the dark web...</p>
+ </div>`;
+
+  setTimeout(function () {
+    const uploadText = document.getElementById("upload-text");
+    uploadText.innerText = "wait please...";
+  }, 1500);
+
+  setTimeout(function () {
+    const modalInner = document.getElementById("modal-inner");
+    modalInner.innerHTML = `<h2>참여해주셔서 감사합니다 ${name}</h2>
+    <p id="last-text">당첨 되시면 ${email}과 ${number}로 </br>보내드리겠습니다.`;
+
+    modalCloseButton.disabled = false;
+  }, 3000);
+});
+
+modalResetBtn.addEventListener("click", function () {
+  contentForm.reset();
 });
