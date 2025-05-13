38219067a546163dcf16cc04675f46d5710c346f
jeongmi-min <alswjdalwjal@naver.com>
2025-05-13 14:11:55 +0900

    main

--- index.js
+++ index.js
@@ -1,51 +1,63 @@
-/* <main> */
+/* <main> / <article> */
 const questionRadios = document.getElementById("question-radios");
-const finalAnswer = document.getElementById("final-answer");
+const finalAnswerBtn = document.getElementById("final-answer-Btn");
+const boxScore = document.getElementById("boxScore");
+const score = document.getElementById("score");
+
+finalAnswerBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  boxScore.style.display = "block";
+  score.textContent = 0;
+
+  const correctAnswers = { q1: 20, q2: 20, q3: 20, q4: 20, q5: 20 };
+
+  let userScore = 0;
+});
 
 /* <modal> */
 
-const modal = document.getElementById("modal");
-const modalCloseButton = document.getElementById("modal-close-btn");
-const contentForm = document.getElementById("content-form");
-const modalText = document.getElementById("modal-text");
-const modalSubmitBtn = document.getElementById("modal-submit-btn");
-const modalResetBtn = document.getElementById("modal-reset-btn");
+// const modal = document.getElementById("modal");
+// const modalCloseButton = document.getElementById("modal-close-btn");
+// const contentForm = document.getElementById("content-form");
+// const modalText = document.getElementById("modal-text");
+// const modalSubmitBtn = document.getElementById("modal-submit-btn");
+// const modalResetBtn = document.getElementById("modal-reset-btn");
 
-// setTimeout(function () {
-//   modal.style.display = "inline";
-// }, 1500);
+// // setTimeout(function () {
+// //   modal.style.display = "inline";
+// // }, 1500);
 
-modalCloseButton.addEventListener("click", function () {
-  modal.style.display = "none";
-});
+// modalCloseButton.addEventListener("click", function () {
+//   modal.style.display = "none";
+// });
 
-modalSubmitBtn.addEventListener("click", function (event) {
-  event.preventDefault();
+// modalSubmitBtn.addEventListener("click", function (event) {
+//   event.preventDefault();
 
-  const contentFormData = new FormData(contentForm);
-  const name = contentFormData.get("fullName");
-  const email = contentFormData.get("email");
-  const number = contentFormData.get("phoneNumber");
+//   const contentFormData = new FormData(contentForm);
+//   const name = contentFormData.get("fullName");
+//   const email = contentFormData.get("email");
+//   const number = contentFormData.get("phoneNumber");
 
-  modalText.innerHTML = `<div class="modal-inner-loading">
-   <img src="images/loading.svg" class="loading">
-   <p id="upload-text">Uploading your data to the dark web...</p>
- </div>`;
+//   modalText.innerHTML = `<div class="modal-inner-loading">
+//    <img src="images/loading.svg" class="loading">
+//    <p id="upload-text">Uploading your data to the dark web...</p>
+//  </div>`;
 
-  setTimeout(function () {
-    const uploadText = document.getElementById("upload-text");
-    uploadText.innerText = "wait please...";
-  }, 1500);
+//   setTimeout(function () {
+//     const uploadText = document.getElementById("upload-text");
+//     uploadText.innerText = "wait please...";
+//   }, 1500);
 
-  setTimeout(function () {
-    const modalInner = document.getElementById("modal-inner");
-    modalInner.innerHTML = `<h2>참여해주셔서 감사합니다 ${name}</h2>
-    <p id="last-text">당첨 되시면 ${email}과 ${number}로 </br>보내드리겠습니다.`;
+//   setTimeout(function () {
+//     const modalInner = document.getElementById("modal-inner");
+//     modalInner.innerHTML = `<h2>참여해주셔서 감사합니다 ${name}</h2>
+//     <p id="last-text">당첨 되시면 ${email}과 ${number}로 </br>보내드리겠습니다.`;
 
-    modalCloseButton.disabled = false;
-  }, 3000);
-});
+//     modalCloseButton.disabled = false;
+//   }, 3000);
+// });
 
-modalResetBtn.addEventListener("click", function () {
-  contentForm.reset();
-});
+// modalResetBtn.addEventListener("click", function () {
+//   contentForm.reset();
+// });
